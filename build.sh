fd16599f5ef5802159847383958953a572af48fd
YazanAhmad-Eng <54546895+YazanAhmad-Eng@users.noreply.github.com>
2020-04-12 17:21:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 sed  "s/{SERVER_NAME}/$server/g" web/indx.txt  |sed  "s/{USER}/$name/g" >indx.txt
 sed -i "s/{TIMESTAMP}/$timestamp/g" indx.txt
 sed -i "s/{GIT_BRANCH}/$branch/g" indx.txt
-sed -i "s/{GIT_REPO}/$GIT_REPO/g" indx.txt
+sed -i "s?{GIT_REPO}?$GIT_REPO?g" indx.txt
 sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" indx.txt
 
 sed  "s~{PWD}~$DIR~g" indx.txt>web/index.html
