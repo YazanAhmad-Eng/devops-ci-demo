ad89f4e9d70073d4e866032f71309deb4089490d
YazanAhmad-Eng <54546895+YazanAhmad-Eng@users.noreply.github.com>
2020-04-12 17:24:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ server=$HOSTNAME
 name=$USER
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 DIR=$(pwd)
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 sed  "s/{SERVER_NAME}/$server/g" web/indx.txt  |sed  "s/{USER}/$name/g" >indx.txt
 sed -i "s/{TIMESTAMP}/$timestamp/g" indx.txt
-sed -i "s/{GIT_BRANCH}/$branch/g" indx.txt
+sed -i "s?{GIT_BRANCH}?$branch?g" indx.txt
 sed -i "s?{GIT_REPO}?$GIT_REPO?g" indx.txt
 sed -i "s/{AZURE_VARIABLE}/$AZURE_VARIABLE/g" indx.txt
+
 
 sed  "s~{PWD}~$DIR~g" indx.txt>web/index.html
