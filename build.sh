a337430fb564a4862a05811d33c34cb93b443818
Yazan <yazan@192.168.1.15>
2020-04-12 15:11:58 +0300

    Added a new tr

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 #print this line 
 echo "Running the Build Sprict"
 echo "I am $HOSTNAME ."
-echo "the Time is $(date %s)"
+echo "the Time is $(date +%Y-%m-%d_%H-%M-%S)"
 echo "the loged in user is $USER "
+
+server=$HOSTNAME
+name=$USER
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+
+
+sed  "s/{SERVER_NAME}/$server/g" web/indx.txt  |sed  "s/{USER}/$name/g" >indx.txt
+sed -i "s/{TIMESTAMP}/$timestamp/g"  web/index.html
